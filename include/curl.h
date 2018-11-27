2062a9e101e7adfe6636eb46d5033d5673ea318b
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-11-27 13:58:34 +0100

    needs a testing if anybody wants

--- include/curl.h
+++ include/curl.h
@@ -15,37 +15,36 @@ size_t write_callback(void *ptr, size_t size, size_t nmemb, FILE *stream) {
 	return written;
 }
 
-/*
-inside function fileDownload() int a options:
-
-- 1 = generates a filename of numbers from 0 up to 999998;
-- 2 = prints useful text for testing curl;
-- 3 = 1 & 2;
-- 0 = default;
-
-*/
-
-void fileDownload(char *url, char *path, char *filename, char *extension, int a) {
+ {
+void fileDownload(char *url, char path[], char filename[], char extension[], int a)
 	chdir(path); // change dir
 	FILE *dest;
-	if (a == 1 || a == 3) // checks if int a is 0, or 3, or 4... If int a has got 1 value, then generates a random filename. If a has got 2 value, prints useful text for testing.
-	{
-		srand(time(NULL)); // sradicates the rand function
-		int random = rand() % 999999; // generating the numbers
-		sprintf(filename, "%d", random);
-		strcat(filename, extension); // collocates the extension after the numbers
-		dest = fopen(filename, "r"); // reading the file
-	} else { // directly reading the file
-	strcat(filename, extension); // collocates the extension after the filename
-	dest = fopen(filename, "r"); // reading the file
+	char buf[1024];
+	
+	if (a == 0) {
+		snprintf(buf, sizeof(buf), "%s.%s", filename, extension);
 	}
 	
-	if (fopen(filename, "r") != NULL) // check the file
-	{
-	    dest = freopen(filename, "wb", NULL); // if the file doesnt exist, reopens fopen with write previligies
-	} else // the file exist
-	{
-		fprintf(stdout, "\n#fileDownload(): file exist already, overwrite? {A} yes, {B} return"); // little warning
+	if (a == 1) {
+		srand(time(NULL)); // sradicates the rand function
+		int random = rand() % 500; // generating the numbers
+		snprintf(buf, sizeof(buf), "010%d.%s", random, extension);
+	}
+	
+	if (buf == NULL) { 
+		perror("\n# Failed: ");
+		goto EXIT;
+	}
+	
+	dest = fopen(buf, "r");
+	
+	if (dest == NULL) {
+		fclose(dest);
+		perror("\n# Failed: ");
+		dest = fopen(buf, "wb");
+	} else { // the file exist
+		printf("\n# File %s exist already, overwrite? [A] Continue, [B] Exit", buf); // little warning
+		
 		while (appletMainLoop()) {
 			hidScanInput();
 			u32 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
@@ -55,34 +54,33 @@ void fileDownload(char *url, char *path, char *filename, char *extension, int a)
 			}
 			if (kDown & KEY_B) {
 				fclose(dest); // as you decided to return, we need to close FILE *stream
-				return;
+				goto EXIT;
 			}
 			
 			gfxFlushBuffers();
             gfxSwapBuffers();
 		}
+		
+		fclose(dest);
+		dest = fopen(buf, "wb");
+		
 	}
 	
-	consoleClear();
 	CURL *curl; 
 	CURLcode res;
 	curl_global_init(CURL_GLOBAL_DEFAULT); // initializing/calling functions
 	curl = curl_easy_init(); // initializing/calling functions
 	
-	fprintf(stdout, "starting libcurl...\n");
+	printf("\n# Starting curl...\n");
     
     if (curl)
 	{
 		appletBeginBlockingHomeButton(0);
-		printf("\n\nURL = %s\nFile = %s%s\n\nDownload...\n", url, path, filename);
+		printf("\n\n* URL = %s\n* File = %s%s\n\n* Download...\n", url, path, buf);
 		curl_easy_setopt(curl, CURLOPT_URL, url); // getting URL from char *url
-		
-	    if (a == 2 || a == 3) {
-			printf("\n* DEBUG MODE *\n");
-			curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); // useful for debugging
-		}
-		
-		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // following HTTP redirects
+		printf("\n* DEBUG MODE\n");
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); // useful for debugging
+		// curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // following HTTP redirects
 		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 20L); // a max of setopt timeout for 20s
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); // skipping cert. verification, if needed
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); // skipping hostname verification, if needed
@@ -91,20 +89,31 @@ void fileDownload(char *url, char *path, char *filename, char *extension, int a)
         res = curl_easy_perform(curl); // perform tasks curl_easy_setopt asked before
 		
 		if (res != CURLE_OK) {
-			printf("\nfailed: %s\n", curl_easy_strerror(res));
+			printf("\n# Failed: %s", curl_easy_strerror(res));
 			fclose(dest);
-			remove(filename);
 		}
 		
 	    appletEndBlockingHomeButton();	
+		
 	    curl_easy_cleanup(curl); // always cleanup
-        fclose(dest); // closing FILE *fp
+
+        fclose(dest); // closing FILE *stream
 	
 	}
 	
 	curl_easy_cleanup(curl);
 	curl_global_cleanup();
-	return;
+	
+	printf("\n\n# Exit by pressing [+] or [HOME]");
+	while(true) {
+		hidScanInput();
+		u32 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+		if (kDown & KEY_PLUS) break;
+	}
+	
+	EXIT:
+	exit(0);
+
 }
 
 #endif
