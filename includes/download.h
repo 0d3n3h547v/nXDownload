c89f9e4e7dcefef780451466b8d1ffc744d14411
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2019-02-21 21:30:22 +0100

    [ADDED] You can enter username & password!
    - [added] Download tmp URL (with user & password)
    - Few fixes i dont remember
    - New updated Icon.jpg!

---
+++ includes/download.h
@@ -0,0 +1,49 @@
+#ifndef _DOWNLOAD_H_
+#define _DOWNLOAD_H_
+
+/* Includes */
+#include <curl/curl.h>
+#include <stdio.h>
+#include <string.h>
+#include <switch.h>
+
+/* Defines */
+#define TIMETYPE double
+#define TIMEOPT CURLINFO_TOTAL_TIME
+#define MINIMAL_PROGRESS_FUNCTIONALITY_INTERVAL     3
+#define STOP_DOWNLOAD_AFTER_THIS_MANY_BYTES         6000
+
+/* Structures */
+struct myprogress {
+  TIMETYPE lastruntime;
+  CURL *curl;
+};
+
+struct a {
+    char        dnld_remote_fname[512];
+    char        dnld_url[512]; 
+    FILE        *dnld_stream;
+    FILE        *dbg_stream;
+    uint64_t    dnld_file_sz;
+} dnld_params;
+
+
+/* Prototypes */
+
+// init curl sockets
+void curlInit(void);
+void curlExit(void);
+
+int nXDownloadUpdate(void);
+void FILE_TRANSFER_FTP(char *url, char path[]);
+bool FILE_TRANSFER_HTTP(char *url, int a);
+bool FILE_TRANSFER_HTTP_TEMPORALY(void);
+int xferinfo(void *p, curl_off_t dltotal, curl_off_t dlnow);
+int older_progress(void *p, double dltotal, double dlnow, double ultotal, double ulnow);
+size_t dnld_header_parse(void *hdr, size_t size, size_t nmemb, void *userdata);
+
+/* this func's is used for auth in case ftp/http requires */
+bool inputPassword(void);
+bool inputUser(void);
+
+#endif
---
+++ menuCUI.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <string.h>
+#include <switch.h>
+#include <stdbool.h>// bool = 1 == true; 0 == false;
+#include "includes/download.h"
+#include "includes/menuCUI.h"
+#include "includes/helper.h"
+
+void title(char *str) {
+	var.half = strlen (str) / 2;
+	var.half_length = var.half;
+	var.final_length = HALF_SCREEN;
+
+	for (; var.half <= var.half_length; var.half--) {
+		var.final_length--;
+		if (var.half <= 0) break;
+	}
+
+	printf ("\x1b[3;%dH%s%s%s", var.final_length, CONSOLE_GREEN, str, CONSOLE_RESET);
+	return;
+}
+
+bool functionExit(void) {
+	printf("\n\n%s%s%s", CONSOLE_GREEN, "Finish!\n", CONSOLE_RESET);
+	printf("\npress [+] to exit");
+	printf("\npress [-] to continue");
+	
+	while(appletMainLoop()) {
+		hidScanInput();
+		u32 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+		
+		if (kDown & KEY_PLUS) return true;
+		if (kDown & KEY_MINUS) return false;
+		consoleUpdate(NULL);
+	}
+	
+	return true;
+}
+
+void menu_options(void) {
+	
+	char *tiles[] = {
+		"Update latest beta nXDownload",
+		"Download from input.txt",
+		"Download temporaly URL",
+		"Download tmp URL (with User & Password)"
+	};
+	
+	initial.m = 5;
+	initial.exit = "Exit";
+	
+	for (initial.n=0; initial.n < COUNT_OF(tiles) ; initial.n++) {
+		printf("\x1b[%d;3H%s", initial.m, tiles[initial.n]);
+		initial.m++;
+	}
+	
+	initial.m = initial.m + 1;
+	printf("\x1b[%d;3H%s", initial.m, initial.exit);
+	
+	return;
+}
+
+bool menu_main(void) {
+	
+	consoleClear();
+	title("nXDownload v1.0b");
+	menu_options();
+	userAppInit();
+	
+	int select = 5;
+	short int bkp_n = initial.n + 5;
+	short int bkp_m = initial.m;
+	
+	while(appletMainLoop()) {
+		
+		title("nXDownload v1.0b");
+		menu_options();
+		
+		/* Look here for colored text >> https://switchbrew.github.io/libnx/console_8h.html */
+		printf("\x1b[%d;1H%s>%s", select, CONSOLE_CYAN, CONSOLE_RESET);
+		
+		hidScanInput();
+		u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+		
+		if (kDown & KEY_DUP) {
+			printf("\x1b[%d;1H ", select);
+			select = select - 1;
+			
+			if (select < 5) select = bkp_m;		// 4 there is nothing
+			if (select == bkp_n) select--;		// between char tiles and exit
+			
+			printf("\x1b[%d;1H%s>%s", select, CONSOLE_CYAN, CONSOLE_RESET);
+		}
+		
+		if (kDown & KEY_DDOWN) {
+			printf("\x1b[%d;1H ", select);
+			select = select + 1;
+			
+			if (select == bkp_n) select++;		// select goes to exit
+			if (select > bkp_m) select = 5;		// over char exit
+			
+			printf("\x1b[%d;1H%s>%s", select, CONSOLE_CYAN, CONSOLE_RESET);
+		}
+		
+		if (kDown & KEY_A) {
+			if (select == 5) {
+				if (nXDownloadUpdate() == true) return true;
+				consoleClear();
+			} else if (select == 6) {
+				if (FILE_TRANSFER_HTTP(NULL, 1) == true) return true;
+				consoleClear();
+			} else if (select == 7) {
+				if (FILE_TRANSFER_HTTP_TEMPORALY() == false)
+					if (FILE_TRANSFER_HTTP(NULL, 2) == true) return true;
+				consoleClear();
+			} else if (select == 8) {
+				
+				if (inputUser() != true) {
+					if (inputPassword() != true) {
+						if (FILE_TRANSFER_HTTP_TEMPORALY() == false) {
+							if (FILE_TRANSFER_HTTP(NULL, 2) == true) return true;
+						}
+					}
+				}
+				
+				consoleClear();
+			} else if (select == 10) return true;
+		}
+
+		
+		if (kDown & KEY_PLUS) return true;
+		
+		consoleUpdate(NULL);
+	}
+	
+	return true;
+}
